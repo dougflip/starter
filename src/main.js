26a9a553981132f9cb8fdf0b340cd0fc6097770f
Doug DiFilippo <ddifilippo@turnitin.com>
2017-01-15 10:05:27 -0500

    Add a small starter component

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
-export default {
-  name: "starter module"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  /* beautify ignore:start */
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+  /* beautify ignore:end */
 }
+
+ReactDOM.render(
+  /* beautify ignore:start */
+  <App />,
+  /* beautify ignore:end */
+  document.querySelector('#main')
+);
